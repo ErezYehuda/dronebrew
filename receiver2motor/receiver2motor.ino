acd02a979e67aae7e45f9858277e73d9f5e562d7
erezyehuda <harrisjweinstein@gmail.com>
2018-12-14 00:37:04 -0500

    Added continuous calibration to controller; restored functionality to receiver (for some reason not working when using cubert_coefficient)

--- receiver2motor/receiver2motor.ino
+++ receiver2motor/receiver2motor.ino
@@ -1,3 +1,4 @@
+
 #include <RF24.h>
 #include <Servo.h>
 
@@ -11,8 +12,8 @@ enum Motor { FRONT_LEFT, FRONT_RIGHT, BACK_LEFT, BACK_RIGHT};
 const int motor_ports[] = {9, 10, 11, 12};
 Servo motors [4];
 
-const int esc_top = 2000;
-const int esc_bott = 710; // Technically, 700 might be acceptible, but it seems to be unreliable for the ESCs to read
+const int esc_top = 2200;
+const int esc_bott = 850; // Technically, 700 might be acceptible, but it seems to be unreliable for the ESCs to read
 const int esc_range = esc_top - esc_bott;
 
 // Some cube-root constants that allow the multiplied throttle distributions to never stray outside a ratio of 75:25
@@ -31,7 +32,8 @@ float distrs[4]; // Distributions of throttle
 RF24 radio(7, 8); // CE, CSN
 const byte address[6] = "00001";
 
-const bool verbose_setup = false, verbose_loop = false;
+const bool verbose_setup = 1, verbose_loop = 1;
+bool config_esc = false;
 
 void setup() {
   if (verbose_setup || verbose_loop)
@@ -47,8 +49,13 @@ void setup() {
   Serial.println("</setup>");
 
   for (int i = 0; i < 10; i++)
-    if (radio.available())
+    if (radio.available()) {
       radio.read(&values, vals_size);
+      if (values[THROTTLE] >= 1020) {
+        config_esc = true;
+        break;
+      }
+    }
     else i -= 1;
 
   if (verbose_setup) {
@@ -56,7 +63,7 @@ void setup() {
     Serial.println(values[THROTTLE]);
   }
 
-  if (values[THROTTLE] >= 1020) {
+  if (config_esc) {
     Serial.println("Configuring ESCs");
     // If it's within 3 of the top Y-position of the left joystick...
     // Write the undistributed throttle to the motors until it gets below 4
@@ -68,7 +75,7 @@ void setup() {
       }
       for (int i = 0; i < 4; i++)
         motors[i].writeMicroseconds(map(values[THROTTLE], 0, 1023, esc_bott, esc_top));
-    } while (values[THROTTLE] > 4);
+    } while (values[THROTTLE] > 10);
 
     // Wait for them to bring the throttle stick up again
     do {
@@ -79,17 +86,19 @@ void setup() {
       if (verbose_setup)
         Serial.println(values[THROTTLE]);
     } while (values[THROTTLE] < 1020);
+    Serial.println("Ending ESC configuration");
+  } else {
+    Serial.println("Skipping ESC configuration");
   }
-  Serial.println("Ending ESC configuration");
 }
 
 void loop() {
   if (radio.available()) {
     radio.read(&values, vals_size);
 
-    if (verbose_loop) {
-      print_channels();
-    }
+    //        if (verbose_loop) {
+    //          print_channels();
+    //        }
 
     // Throttle->All
     // Aileron=Roll->Left vs. Right
@@ -100,23 +109,35 @@ void loop() {
     float throttle = (values[THROTTLE] - 512) * 2  / 1023.0;
     if (throttle < 0) throttle = 0;
     // Setting it up so that the product of aileron, elevator, and rudder for any given motor will be [0.25,0.75]
-    float aileron  = values[AILERON]  / cubert_coefficient + cubert25;
-    float elevator = values[ELEVATOR] / cubert_coefficient + cubert25;
-    float rudder   = values[RUDDER]   / cubert_coefficient + cubert25;
+    float aileron  = values[AILERON]  / 1023.0 * inter_cubert + cubert25;
+    float elevator = values[ELEVATOR] / 1023.0 * inter_cubert + cubert25;
+    float rudder   = values[RUDDER]   / 1023.0 * inter_cubert + cubert25;
 
     for (int i = 0; i < 4; i++) {
       distrs[i] = throttle;
     }
 
+    //    print_dists();
+
     distrs[FRONT_RIGHT] *= aileron;
     distrs[BACK_RIGHT]  *= aileron;
-    distrs[FRONT_LEFT]  *= (cubert_sum - aileron );
-    distrs[BACK_LEFT]   *= (cubert_sum - aileron );
+    distrs[FRONT_LEFT]  *= cubert_sum - aileron;
+    distrs[BACK_LEFT]   *= cubert_sum - aileron;
+
+    //  Serial.println(aileron);
+    //  Serial.print("cubert_sum - ail:");
+    //  Serial.print(cubert_sum);
+    //  Serial.print(" - ");
+    //  Serial.print(aileron);
+    //  Serial.print(" = ");
+    //  Serial.println(cubert_sum-aileron);
+
+    //    print_dists();
 
     distrs[BACK_LEFT]   *= elevator;
     distrs[BACK_RIGHT]  *= elevator;
-    distrs[FRONT_LEFT]  *= (cubert_sum - elevator);
-    distrs[FRONT_RIGHT] *= (cubert_sum - elevator);
+    distrs[FRONT_LEFT]  *= cubert_sum - elevator;
+    distrs[FRONT_RIGHT] *= cubert_sum - elevator;
 
     distrs[BACK_LEFT]   *= rudder;
     distrs[FRONT_RIGHT] *= rudder;
@@ -138,9 +159,10 @@ void loop() {
     //    Serial.println();
     //    Serial.println((distrs[0] + distrs[1] + distrs[2] + distrs[3]));
 
-    //    if (verbose_loop) {
-    //      print_mico_dists();
-    //    }
+    if (verbose_loop) {
+      //      print_dists();
+      print_mico_dists();
+    }
 
   }
 }
