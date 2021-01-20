d6f93d8963cd4ff2482813d54f1f3f2d821ac049
ErezYehuda <harrisjweinstein@gmail.com>
2021-01-20 17:12:22 -0500

    Use simpler calculations for joystick bounding

--- calibrations/joystick_bounding/joystick_bounding.ino
+++ calibrations/joystick_bounding/joystick_bounding.ino
@@ -1,7 +1,5 @@
 const int elevator_in  = A2; // X Input Pin of Analog 2
 const int aileron_in   = A3; // Y Input Pin of Analog 3
-const float two_pi = 2 * M_PI;
-
 
 float values[4];
 
@@ -13,23 +11,15 @@ void loop() {
   float x = (analogRead(aileron_in) - 512) / 512.0;
   float y = (analogRead(elevator_in) - 512) / 512.0;
 
-  float radian = atan2(y, x);
-  if (radian < 0)
-    radian = two_pi + radian;
+  float z = sqrt(x * x + y * y);
 
-  float bound_x = cos(radian);
-  float bound_y = sin(radian);
+  float bound_x = x / z;
+  float bound_y = y / z;
 
-  if (x / bound_x > 1 || y / bound_y > 1) {
-    Serial.print("OOB: " + (String)x + "," + (String) y + " outside +-" + (String)bound_x + "," + (String)bound_y + " (Radians " + (String)radian + ")");
-    float snapradian = atan2(bound_y, bound_x);
-    if (snapradian < 0)
-      snapradian = two_pi + snapradian;
-    Serial.println("---> " + (String)bound_x + "," + (String)bound_y + " (Radians " + (String)snapradian + ")");
+  if (z > 1) {
+    Serial.println("OOB: " + (String)x + "," + (String) y + " outside +-" + (String)bound_x + "," + (String)bound_y);
   }
   else {
-    Serial.println("     " + (String)x + "," + (String) y + "  inside +-" + (String)bound_x + "," + (String)bound_y + " (Radians " + (String)radian + ")");
+    Serial.println("     " + (String)x + "," + (String) y + "  inside +-" + (String)bound_x + "," + (String)bound_y);
   }
-
-
 }
